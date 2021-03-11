b0458f2fc4ca7af458162f4e058b85c8d11f07e9
Ankur Gupta <rukna1000@gmail.com>
2021-03-11 12:32:42 -0500

    Create overarching test

--- src/scheduler.py
+++ src/scheduler.py
@@ -21,27 +21,34 @@ class Level:
 class Arena:
   level: Level
   time_control: TimeControl
-  start_date: datetime.datetime
+  start_datetime: datetime.datetime
 
   duration: int = 120
   min_rated: int = 10
   
   def prepare_request(self) -> Dict[Text, Union[Text, int, bool]]:
-    return {
+    request = {
       'name': '{} {}+{} Chess960'.format(
         self.level.name, self.time_control.clock, self.time_control.increment),
       'clockTime': self.time_control.clock,
       'clockIncrement': self.time_control.increment,
       'minutes': self.duration,
+      'startDate': int(self.start_datetime.timestamp()),
       'variant': 'chess960',
       'description': 'Daily Chess960 Arena. Times cycle daily so that everyone'
         'gets a chance to play their favorite time control. Please put all'
         'suggestions in the forum! Have fun :)',
       'conditions.teamMember.teamId': 'chess960',
-      'conditions.minRating.rating': self.level.min_elo,
-      'conditions.maxRating.rating': self.level.max_elo,
       'conditions.nbRatedGame.nb': self.min_rated,
     }
+
+    if self.level.min_elo:
+      request['conditions.minRating.rating'] = self.level.min_elo
+
+    if self.level.max_elo:
+      request['conditions.maxRating.rating'] = self.level.max_elo,
+
+    return request
   
 
 TIME_CONTROLS = [
--- src/test_scheduler.py
+++ src/test_scheduler.py
@@ -1,11 +1,29 @@
-from . import scheduler
-#import scheduler
-
+#from . import scheduler
+import scheduler
 import unittest
 
+import datetime
 
-class SchedulerTestCase(unittest.main):
-  pass
+
+class SchedulerTestCase(unittest.TestCase):
+  def test_arena_request_overall(self):
+    clock = 5
+    increment = 3
+    min_elo = 69
+    max_elo = 420
+
+    start_datetime = datetime.datetime(2020, 4, 20, 4, 20, 0)
+    start_timestamp = 1587370800 # https://www.epochconverter.com/
+    
+    a = scheduler.Arena(
+      scheduler.Level(min_elo, max_elo),
+      scheduler.TimeControl(clock, increment), start_datetime)
+    r = a.prepare_request()
+    
+    self.assertEqual(r['clockTime'], clock)
+    self.assertEqual(r['clockIncrement'], increment)
+    self.assertEqual(r['minutes'], 120)
+    self.assertEqual(r['startDate'], start_timestamp)
 
 
 if __name__ == '__main__':
